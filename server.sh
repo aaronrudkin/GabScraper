aa3d9ab3cf4a89068bc8e0ca89e392078c685a73
Aaron Rudkin <junk_signups@rudkin.ca>
2017-04-24 16:12:07 -0700

    Added server setup shell script.

---
+++ server.sh
@@ -0,0 +1,9 @@
+passwd
+tmux
+apt-get install unzip python python-pip
+pip install mechanize html5lib
+wget https://github.com/aaronrudkin/GabScraper/archive/master.zip
+unzip master.zip
+cd GabScraper-master
+mv auth_blank.json auth.json
+nano auth.json
